4f832dcd3c982ebe82c382a0d7da3205d1e4d686
+ <+>
2022-11-22 23:20:48 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage = 'Welcome to Flatbook, + currentUser' + 'Grace Hopper!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, {currentuser.toUpperCase(}' + 'GRACE HOPPER!'
+const shortGreeting = 'Welcome,  ${currentUser.slice(0,1)}' + 'GRACE HOPPER!'
